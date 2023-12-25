7ffc9946e09a8c81efe2114b8f75364b5c864dfd
Nodirbek-Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2023-12-25 13:38:41 +0500

    Add project files.

---
+++ Test/PingService.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Test;
+
+public class PingService : BackgroundService
+{
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var response = await PingToUrl();
+            if (response.IsSuccessStatusCode)
+            {
+                SendToUseFromTelegramBot("Server is working");
+            }
+            else
+            {
+                SendToUseFromTelegramBot($"Server is not working: {response.StatusCode}");
+            }
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+
+    public async Task<HttpResponseMessage> PingToUrl()
+    {
+        var client = new HttpClient();
+        return await client.GetAsync("https://savdo.uzavtosanoat.uz/t/ap/stream/ph&models");
+    }
+
+    public async void SendToUseFromTelegramBot(string message)
+    {
+        var client = new HttpClient();
+        string token = "6566749542:AAGbDcOEnTywiTjdoAJ3_yp41_dHtxw7Dz4";
+        long userId = 1614764463;
+        await client.GetAsync($"https://api.telegram.org/bot{token}/sendMessage?chat_id={userId}&text={message}");
+    }
+}
