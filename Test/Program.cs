646e5fcab2175e8d008ae2afeeb31ab9a3660ec7
Nodirbek-Abdulaxadov <nbkabdulaxadov2000@gmail.com>
2023-12-25 13:46:03 +0500

    sht

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,9 +1,12 @@
+using Test;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<PingService>();
 
 var app = builder.Build();
 
